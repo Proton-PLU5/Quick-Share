d13f3328438d7b997f79554c3e34f6591a362b77
_ProtonPLUS <mcib088@aucklanduni.ac.nz>
2025-01-09 19:57:52 +1300

    Update MainScene.java

--- src/main/java/me/mathewcibi/quickshare/scenes/MainScene.java
+++ src/main/java/me/mathewcibi/quickshare/scenes/MainScene.java
@@ -15,6 +15,7 @@ import me.mathewcibi.quickshare.utils.NetworkServer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -107,11 +108,7 @@ public class MainScene extends Scene {
         // Device IP
         String ip = "ERROR GETTING IP";
         try {
-            URL whatismyip = new URL("http://checkip.amazonaws.com");
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    whatismyip.openStream()));
-
-            ip = in.readLine();
+            ip = InetAddress.getLocalHost().getHostAddress();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
